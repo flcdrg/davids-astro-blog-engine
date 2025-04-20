1decd83c971162695035c3bc5b89b4faea2dc765
David Gardiner <david@gardiner.net.au>
2025-04-20 19:38:33 +0930

    Set content type for rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,33 +1,34 @@
 import atom from 'astrojs-atom';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
 
 export async function GET(context) {
   const posts = await getCollection("blog");
 
-  // return rss({
-  //   title: 'David Gardiner',
-  //   description: 'A blog of software development, .NET and other interesting things',
-  //   site: context.site,
-  //       items: posts.map((post) => ({
-  //     title: post.data.title,
-  //     pubDate: post.data.date,
-  //     description: post.data.description,
-  //     link: `/${post.id}.html`,
-  //   })),
-  //   customData: `<language>en-au</language>`,
-  // });
+  const sortedPosts = posts.sort((a, b) => new Date(b.data.date) - new Date(a.data.date));
+  const postsToInclude = sortedPosts.slice(0, 10); // Get the latest 10 posts
+
+
 
   return atom({
-    id: 'https://david.gardiner.net.au',
+    id: context.site.toString(),
     title: 'David Gardiner',
     updated: new Date().toISOString(),
     description: 'A blog of software development, .NET and other interesting things',
     site: context.site,
-    entry: posts.map((post) => ({
-      id: `https://david.gardiner.net.au/${post.id}`,
+    entry: postsToInclude.map((post) => ({
+      id: `${(new URL(post.id, context.site)).toString()}`,
       updated: post.data.date,
       title: post.data.title,
-      content: post.data.description
+      content: {
+        type: 'html',
+        value: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+        }),
+      }
     })),
   });
 }
