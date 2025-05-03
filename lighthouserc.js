2c4309eca3aa40dad5550127a6a80dd6d3b58a78
David Gardiner <david@gardiner.net.au>
2025-05-03 14:37:54 +0930

    PowerShell script to process Lychee json and generate better Markdown report

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,6 +1,12 @@
 export const ci = {
     collect: {
         staticDistDir: './dist',
+        url: [
+            'http://localhost/index.html',
+            'http://localhost/speaking.html',
+            'http://localhost/2025.html',
+            'http://localhost/2025/04/azure-function-isolated.html'
+        ]
     },
     upload: {
         target: 'temporary-public-storage',
